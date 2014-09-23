f2ce21407ba35f93b907884bcbaad374a19f48cd
Yoshiki Yamada <nyamadandan@gmail.com>
2014-09-23 23:07:09 +0900

    add 2D plot

--- lib/noise.dart
+++ lib/noise.dart
@@ -3,6 +3,15 @@ library noise;
 import "dart:math" as Math;
 import "dart:typed_data";
 
+num _cubicInterpolate(num x, num v0, num v1, num v2, num v3) {
+  num p = (v3 - v2) - (v0 - v1);
+  num q = (v0 - v1) - p;
+  num r = v2 - v0;
+  num s = v1;
+
+  return p * x * x * x + q * x * x + r * x + s;
+}
+
 List<num> createRandomNoise(int length, int wavelength, num func(int index)) {
   int sample_length = length ~/ wavelength + 5;
 
@@ -30,32 +39,112 @@ List<num> createRandomNoise(int length, int wavelength, num func(int index)) {
     num v3 = samples[sample_index + 3];
     double x = (index % wavelength) / wavelength;
 
-    num p = (v3 - v2) - (v0 - v1);
-    num q = (v0 - v1) - p;
-    num r = v2 - v0;
-    num s = v1;
+    return _cubicInterpolate(x, v0, v1, v2, v3);
+  });
+}
 
-    return p * x * x * x + q * x * x + r * x + s;
+List<List<num>> createRandomNoise2D(int length, int wavelength, num func(int w, int h)) {
+  int sample_length = length ~/ wavelength + 5;
+
+  if (length % wavelength != 0) {
+    sample_length++;
+  }
+
+  List <List<num>> samples = new List<List<num>>.generate(sample_length, (int h){
+    return new List<num>.generate(sample_length, (int w){
+      return func(w - 2, h - 2);
+    }, growable: false);
+  }, growable: false);
+
+  samples = new List<List<num>>.generate(sample_length - 2, (int h){
+    return new List<num>.generate(sample_length - 2, (int w){
+      return (samples[h][w] + samples[h][w+2] + samples[h+2][w] + samples[h+2][w+2]) * 0.0625
+        + (samples[h+1][w] + samples[h+1][w+2] + samples[h+2][w+1] + samples[h][w+1]) * 0.125
+        + samples[h+1][w+1] * 0.25;
+    }, growable: false);
+  }, growable: false);
+
+  return new List<List<num>>.generate(length, (int h){
+    return new List<num>.generate(length, (int w){
+      int i_w = w ~/ wavelength;
+      int i_h = h ~/ wavelength;
+      double x = (w % wavelength) / wavelength;
+      double y = (h % wavelength) / wavelength;
+
+      List<num> v = new List<num>.generate(4, (int i){
+        num v0_x = samples[i_h+i][i_w+0];
+        num v1_x = samples[i_h+i][i_w+1];
+        num v2_x = samples[i_h+i][i_w+2];
+        num v3_x = samples[i_h+i][i_w+3];
+        return _cubicInterpolate(x, v0_x, v1_x, v2_x, v3_x);
+      });
+
+      return _cubicInterpolate(y, v[0], v[1], v[2], v[3]);
+    });
   });
 }
 
 List<num> createPerlinNoise(int length, int wavelength, num persistence, num func(int index, num amplitude)) {
   List<num> buffer = new List<num>.generate(length, (int index) =>  0, growable: false);
+
   int count = 0;
+  num total_amplitude = 0.0;
   while(wavelength > 0) {
     num amplitude = Math.pow(persistence, count);
 
     List<num> noise = createRandomNoise(length, wavelength, (int index){
-      return amplitude * func(index, amplitude);
+      return func(index, amplitude);
     });
 
     for(int i = 0; i < length; i++) {
       buffer[i] = buffer[i] + noise[i];
     }
 
+    total_amplitude += amplitude;
     wavelength = wavelength ~/ 2;
     count = count + 1;
   }
 
+  for(int i = 0; i < length; i++){
+    buffer[i] /= total_amplitude;
+  }
+
   return buffer;
 }
+
+List<List<num>> createPerlinNoise2D(int length, int wavelength, num persistence, num func(int w, int h, num amplitude)) {
+  List<List<num>> buffer = new List<List<num>>.generate(length, (int h){
+    return new List<num>.generate(length, (int w){
+      return 0;
+    }, growable: false);
+  }, growable: false);
+
+  int count = 0;
+  num total_amplitude = 0.0;
+  while(wavelength > 0) {
+    num amplitude = Math.pow(persistence, count);
+
+    List<List<num>> noise = createRandomNoise2D(length, wavelength, (int w, int h){
+      return func(w, h, amplitude);
+    });
+
+    for(int h = 0; h < length; h++) {
+      for(int w = 0; w < length; w++) {
+        buffer[h][w] += noise[h][w];
+      }
+    }
+
+    total_amplitude += amplitude;
+    wavelength = wavelength ~/ 2;
+    count = count + 1;
+  }
+
+  for(int h = 0; h < length; h++){
+    for(int w = 0; w < length; w++){
+      buffer[h][w] /= total_amplitude;
+    }
+  }
+
+  return buffer;
+}
+
--- lib/noise_canvas.dart
+++ lib/noise_canvas.dart
@@ -6,11 +6,13 @@ import "package:simplot/simplot.dart" as simplot;
 import "package:noise/noise.dart";
 
 import "dart:math" show Random;
+import "dart:typed_data";
 import "dart:html";
 
 @CustomTag("x-noise-canvas")
 class NoiseCanvas extends PolymerElement {
   Element _graphContainer;
+  CanvasRenderingContext2D _ctx;
 
   Random _random = new Random();
 
@@ -24,6 +26,9 @@ class NoiseCanvas extends PolymerElement {
     ShadowRoot root =  this.shadowRoot;
     this._graphContainer = root.querySelector("#simPlotQuad");
     this.seed = this._random.nextInt(0xffff).toString();
+
+    CanvasElement canvas = root.querySelector("#canvas");
+    this._ctx = canvas.getContext("2d");
 
     this.plot();
   }
@@ -46,14 +51,34 @@ class NoiseCanvas extends PolymerElement {
     }
 
     Random random = new Random(num.parse(this.seed).toInt());
-
     _graphContainer.childNodes.toList().forEach((Element element) => element.remove());
     simplot.plot(
       createPerlinNoise(0x100, wavelength, persistence, (int index, num amplitude){
-        return random.nextInt(0xff);
+        return amplitude * random.nextInt(0xff);
       })
       ,
       shadow: this._graphContainer
     );
+
+    Random random2d = new Random(num.parse(this.seed).toInt());
+    List<List<num>> noise2d = createPerlinNoise2D(0xff, wavelength, persistence, (int w, int h, int amplitude){
+      return amplitude * random.nextInt(0xff);
+    });
+
+    ImageData image_data = this._ctx.createImageData(0xff, 0xff);
+    for(int h = 0; h < 0xff; h++) {
+      for(int w = 0; w < 0xff; w++) {
+        int i = (0xff * h + w) * 4;
+        int color = noise2d[h][w].round();
+        Uint8List buffer = new Uint8List.fromList([
+          color,
+          color,
+          color,
+          0xff
+        ]);
+        image_data.data.setRange(i, i + 4, buffer);
+      }
+    }
+    this._ctx.putImageData(image_data, 0, 0);
   }
 }
